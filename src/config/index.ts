28351344f11879b57324c6a4abb419cb52dce3e1
naimhossen1929 <naimhossen1929@gmail.com>
2026-07-02 01:56:28 +0600

    Successfully connected serverless neon database

---
+++ src/config/index.ts
@@ -0,0 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config({ quiet: true });
+const config = {
+  port: process.env.PORT as string,
+  database_url: process.env.CONNECTION_STRING as string,
+};
+
+export default config;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,36 @@
-import express from "express";
-const app = express();
-const port = 3000;
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+import config from "./config";
+import { neon } from "@neondatabase/serverless";
+const app: Application = express();
 
-app.get("/", (req, res) => {
-  res.send("DevPulse server");
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "DevPulse Server",
+    author: "Naim Hossen",
+  });
+});
+app.post("/", async (req: Request, res: Response) => {
+  // console.log(req.body);
+  const { name, email, password } = req.body;
+  res.status(201).json({
+    message: "Created",
+    data: {
+      name,
+      email,
+    },
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+const sql = neon(config.database_url);
+
+app.listen(config.port, () => {
+  console.log(`Example app listening on port ${config.port}`);
 });
