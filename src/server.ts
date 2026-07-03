a3b9dcd0fb2165aa42c3a67f799e5f2d5f7593dc
naimhossen1929 <naimhossen1929@gmail.com>
2026-07-03 22:46:41 +0600

    Successully created the first user

--- src/server.ts
+++ src/server.ts
@@ -19,17 +19,47 @@ app.get("/", (req: Request, res: Response) => {
 });
 app.post("/", async (req: Request, res: Response) => {
   // console.log(req.body);
-  const { name, email, password } = req.body;
-  res.status(201).json({
-    message: "Created",
-    data: {
-      name,
-      email,
-    },
-  });
+  const { name, email, password, role } = req.body;
+  try {
+    const result = await sql`
+    INSERT INTO users(name,email,password,role) VALUES(${name},${email},${password},${role}) RETURNING *
+    `;
+    console.log(result);
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      data: result,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      error: error,
+    });
+  }
 });
 
 const sql = neon(config.database_url);
+
+const initDB = async () => {
+  try {
+    await sql`
+    CREATE TABLE IF NOT EXISTS users(
+    id SERIAL PRIMARY KEY, 
+    name VARCHAR(20) NOT NULL,
+    email VARCHAR(30) UNIQUE NOT NULL,
+    password VARCHAR(20) NOT NULL,
+    role VARCHAR(15) DEFAULT 'contributor',
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+    )
+    `;
+    console.log("Database connected successfully!");
+  } catch (error) {
+    console.log(error);
+  }
+};
+initDB();
 
 app.listen(config.port, () => {
   console.log(`Example app listening on port ${config.port}`);
